d15a86b97b190c37069ec0e355d177cbb535851b
jjjvvvvv <jonathanvrg554@gmail.com>
2020-06-22 08:46:44 -0400

    Conditional visibility of buttons added upon reaching the final stories that only need one button.

--- lib/main.dart
+++ lib/main.dart
@@ -65,8 +65,8 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
                   child: FlatButton(
                     onPressed: () {
                       setState(() {
@@ -90,7 +90,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -41,8 +41,6 @@ class StoryBrain {
 
   int _storyNumber = 0;
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
   getStory() {
     return _storyData[_storyNumber].storyTitle;
   }
@@ -77,5 +75,11 @@ class StoryBrain {
     _storyNumber = 0;
   }
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
